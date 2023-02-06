af9a4564ccb4e28ee07aaa485c9b4ee803407f86
Nilton S Ferreira <nsfgeo@hotmail.com>
2023-02-05 21:53:04 -0300

    desenvolvido a função de editar

--- Program.cs
+++ Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que você deseja fazer? ");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - Criar novo arquivo");
+            Console.WriteLine("0 - Sair");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0:
+                    System.Environment.Exit(0);     // Esse comando é para sair do ambiente que em que estou, no caso, ambiente de console.
+                    break;
+                case 1:
+                    Abrir();
+                    break;
+                case 2:
+                    Editar();
+                    break;
+                default:
+                    Menu();
+                    break;
+            }
+
+            static void Abrir() { }
+
+            static void Editar()
+            {
+                Console.Clear();
+                Console.WriteLine("Digite seu texto abaixo (ESC para sair): ");
+                Console.WriteLine("------------------------");
+                string text = "";
+
+                while (Console.ReadKey().Key != ConsoleKey.Escape)   // o laço de repetição repete até que a tecla ESC seja apertada
+                {
+
+                }
+            }
+        }
+    }
+}
